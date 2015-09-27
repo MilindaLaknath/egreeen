6d16ddc8081f555d303ac055cc3aa88a2df9194f
Milinda <Milinda@DESKTOP-9B029VG>
2015-09-27 22:15:31 +0530

    Git Clone and Push.

--- class/db.php
+++ class/db.php
@@ -1,7 +1,6 @@
 <?php
 // 1. Create a database connection
 $connection = mysqli_connect("localhost", "root", "", "greeen");
-//$connection = mysqli_connect("localhost", "milindal", "M!1!n6aL", "green");
 // Test if connection succeeded
 if (mysqli_connect_errno()) {
     die("Database connection failed: " .
